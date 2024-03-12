2a7d003035f8094c938a98b02084a0a3d45dd8b1
Crowdbotics <team@crowdbotics.com>
2024-03-12 22:51:37 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-sun-46982",
+  url: "https://royal-sun-46982.botics.co",
+  api: "https://royal-sun-46982.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
